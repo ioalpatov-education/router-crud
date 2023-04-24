20d7307c8d663d7fa0c572a57fca077ad75f60e8
IlyaAlpatov <alpatov30@gmail.com>
2023-04-25 00:10:21 +0300

    добавлен контекст и роутинг

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,43 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import PostCreationPage from "./components/PostCreationPage";
+import PostPage from "./components/PostPage";
+import { createContext, useState } from "react";
+import axios from "axios";
+
+export const PostsContext = createContext(null);
 
 function App() {
-  return <div className="app"></div>;
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_URL}/posts`
+      );
+
+      setPosts(!data ? [] : data);
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
+  return (
+    <div className="app">
+      <PostsContext.Provider value={{ posts, setPosts, getPosts }}>
+        <Router>
+          <Routes>
+            <Route element={<Home />} path="/"></Route>
+            <Route path="posts">
+              <Route element={<PostCreationPage />} path="new" />
+              <Route element={<PostPage />} path=":id" />
+            </Route>
+          </Routes>
+        </Router>
+      </PostsContext.Provider>
+    </div>
+  );
 }
 
 export default App;
